1bb49fb791e1f470495034713dbac8c085d84466
Helloisa22 <helloisa.betriz44@gmail.com>
2021-02-18 15:10:15 -0300

    Terminada as rotas entre components

--- BlogPessoal/src/app/app-routing.module.ts
+++ BlogPessoal/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntradaComponent } from './entrada/entrada.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'entrada', pathMatch: 'full'},
+  {path: 'entrada', component: EntradaComponent},
+  {path: 'cadastrar', component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
