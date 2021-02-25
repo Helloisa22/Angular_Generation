1608b4b0193450fd0eb68b50cf53bfd1fb7051cc
Helloisa22 <helloisa.betriz44@gmail.com>
2021-02-24 23:49:53 -0300

    Componente Tema finalizado rotas, layaut

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntradaComponent } from './entrada/entrada.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'entrada', pathMatch: 'full'}, //ALTEREI AQUI
   {path: 'entrada', component: EntradaComponent},
   {path: 'cadastrar', component: CadastrarComponent},
-  {path: 'inicio', component: InicioComponent}
+  {path: 'inicio', component: InicioComponent},
+  {path: 'tema',  component: TemaComponent}
 ];
 
 @NgModule({
